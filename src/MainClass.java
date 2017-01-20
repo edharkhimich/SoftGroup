ec107572ee380155af952581e28638ceb8d8a59d
Edgar Khimich <edgarkhimich@gmail.com>
2017-01-21 00:09:54 +0200

    Fibanacci

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
 
 public class MainClass {
 
+    private static int current = 1;
+    private static int previous = 0;
+    private static int result;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        fibanacciCounter(5);
+    }
+
+
+    private static void fibanacciCounter(int incomingNumber) {
+        if (incomingNumber != 0) {
+            while (current != incomingNumber) {
+                if (incomingNumber == 1) {
+                    System.out.println("1");
+                } else if (incomingNumber == 2) {
+                    System.out.println("1");
+                } else {
+                    result = current + previous;
+                    previous = current;
+                    current = result;
+                    System.out.println(result);
+                }
+            }
+        } else {
+            throw new NullPointerException("Please write correct number. The number cannot be 0");
+        }
     }
 }
